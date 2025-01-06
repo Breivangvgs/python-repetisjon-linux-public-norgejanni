7fa5fbb3ee8090c93ad6f5fc5148eb10336985ce
norgejanni <98100367+norgejanni@users.noreply.github.com>
2025-01-06 13:24:53 +0100

    initial commit

---
+++ halveringsmetoden.py
@@ -0,0 +1,32 @@
+import random
+
+playing = True
+
+def ogList(maxNumber):
+    leList = []
+    for i in range(1,maxNumber+1):
+        leList.append(i)
+    return leList
+def rightOrWrong(guess, currentList):
+    feedback = input("HIGHER, LOWER OR CORRECT?")
+    if feedback.lower().startswith("c"):
+        print("LETSGOOOO")
+    elif feedback.lower().startswith("h"):
+        del currentList[0:guess]
+    elif feedback.lower().startswith("l"):
+        del currentList[guess-1:-1]
+    return currentList
+
+
+while playing:
+    print("GUESS THE NUMBER")
+    maxNum = int(input("INSERT RANGE: 1-"))
+    numList = ogList(maxNum)
+    print(numList)
+    while True:
+        guess = numList[int(len(numList)/2)-1]
+        print(f"MY GUESS IS {guess}")
+        numList = rightOrWrong(guess, numList)
+        print(numList)
+    
+    
